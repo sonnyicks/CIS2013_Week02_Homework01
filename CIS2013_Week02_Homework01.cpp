878983e51f2956374c280243a966acb28229d01a
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-01-28 14:43:46 -0600

    All operators added

--- CIS2013_Week02_Homework01.cpp
+++ CIS2013_Week02_Homework01.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main (){
-	int first_number;
-	int second_number;
+	float first_number;
+	float second_number;
 	char math_sign;
 	float calculated_solution;
 	bool run_again = true;
+	bool invalid_response = true;
 	
 	cout << "Please enter your first number: "; 
 	cin >> first_number;
@@ -15,12 +16,27 @@ int main (){
 	cout << "Would you like to add, subtract, multiply or divide the two numbers? \n";
 	cout << "For subtraction, the second number will be taken away from the first, \n";
 	cout << "and for the division, the first number will be divided by the first. \n" << "\n";
+	while(invalid_response==true){
 	cout << "Enter +,-,*, or /.  "; 
 	cin >> math_sign;
 	//if condition for addition
 	if (math_sign == '+'){
-		calculated_solution=(first_number+second_number);
-	cout << "The solution is " << calculated_solution;}
+	calculated_solution=(first_number+second_number);
+	invalid_response=false;}
+	//in condition for subtraction
+	if (math_sign == '-'){
+	calculated_solution=(first_number+second_number);
+	invalid_response=false;}
+	//if condition for multiplication
+	if (math_sign == '*'){
+	calculated_solution=(first_number*second_number);
+	invalid_response=false;}
+	//if condition for division
+	if (math_sign == '/'){
+	calculated_solution=(first_number/second_number);
+	invalid_response=false;}
+	if (invalid_response==false){cout << "The solution is " << calculated_solution;}
+	else{cout << "Invalid response - \n";}}
 	
 	return 0;
 }
